6a58c212e1d74d65a0f610d65c6b6c81e610db8b
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:34:47 +0800

    Add project files.

---
+++ Piloton_Decision Statement/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Number 1: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Number 2: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 == num2) Console.WriteLine("{0}=={1}", num1, num2);
+        if (num1 > num2) Console.WriteLine($"{num1}>{num2}");
+        if (num1 < num2) Console.WriteLine($"{num1}<{num2}");
+    }
+}
